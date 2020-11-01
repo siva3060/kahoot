dc526d4c0be8a37ce9bef4627f85b6fa00f5e516
ramya <sriyegu>
2020-11-01 20:34:37 +0530

    implemented game pin UI screen

---
+++ src/Kahoot.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import GamePin from './components/GamePin';
+function Kahoot() {
+  return (
+    <div className="App">
+      <GamePin />
+    </div>
+  );
+}
+
+export default Kahoot;
