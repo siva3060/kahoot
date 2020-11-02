e58e1608c1a0a36494642bb9ea229c95a4e0997b
ramya <sriyegu>
2020-11-02 12:42:00 +0530

    cleaned local dir

--- src/Kahoot.js
+++ src/Kahoot.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import GamePin from './components/GamePin';
+import GamePin from './components/Home/GamePin';
+import GamePinDisplay from './components/Home/GamePinDisplay';
+import {  BrowserRouter as Router,  Switch,  Route,  Link } from "react-router-dom";
+
 function Kahoot() {
   return (
-    <div className="App">
-      <GamePin />
+<div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+           
+            <li>
+              <Link to="/GamePin">GamePin</Link>
+            </li>
+            <li>
+              <Link to="/GamePinDisplay">GamePinDisplay</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/GamePin">
+            <GamePin />
+          </Route>
+          <Route path="/GamePinDisplay">
+            <GamePinDisplay />
+          </Route>
+          
+        </Switch>
+      </div>
+    </Router>
     </div>
-  );
+  )
 }
 
 export default Kahoot;
