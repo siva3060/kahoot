cbe026127bab85506485e34d86187d0842d9cfb7
siva <siva3060@gmail.com>
2021-01-22 22:46:37 +0530

    Restructuirng app for user flow

--- src/Kahoot.js
+++ src/Kahoot.js
@@ -8,59 +8,64 @@ import Questiondisplay_layout from './components/Game/Questiondisplay_layout';
 import {  BrowserRouter as Router,  Switch,  Route,  Link } from "react-router-dom";
 
 function Kahoot() {
-  return (
-<div>
-
-
-<AdminLayout />
-
-{/*
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/GamePin">GamePin</Link>
-            </li>
-            <li>
-              <Link to="/GamePinDisplay">GamePinDisplay</Link>
-            </li>
-            <li>
-              <Link to="/Lobby">Lobby</Link>
-            </li>
-            <li>
-              <Link to="/Question">Question</Link>
-            </li>
-            <li>
-              <Link to="/New_Question">New Question</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/GamePin">
-            <GamePin />
-          </Route>
-          <Route path="/GamePinDisplay">
-            <GamePinDisplay />
-          </Route>
-          <Route path="/Lobby">
-            <Lobby />
-          </Route>
-          <Route path="/Question">
-            <Questiondisplay_layout />
-          </Route>
-
-          <Route path="/New_Question">
-            <Newquestion_layout />
-          </Route>
-          
-          
-        </Switch>
-      </div>
-    </Router> */}
-    </div>
-  )
+  return(<GamePin />);
 }
-
 export default Kahoot;
+
+  
+//TODO :
+// When do i need div
+//   return (
+// <div>
+
+
+// {/* <AdminLayout /> */}
+
+// {
+//     <Router>
+//       <div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/GamePin">GamePin</Link>
+//             </li>
+//             <li>
+//               <Link to="/GamePinDisplay">GamePinDisplay</Link>
+//             </li>
+//             <li>
+//               <Link to="/Lobby">Lobby</Link>
+//             </li>
+//             <li>
+//               <Link to="/Question">Question</Link>
+//             </li>
+//             <li>
+//               <Link to="/New_Question">New Question</Link>
+//             </li>
+//           </ul>
+//         </nav>
+
+//         <Switch>
+//           <Route path="/GamePin">
+//             <GamePin />
+//           </Route>
+//           <Route path="/GamePinDisplay">
+//             <GamePinDisplay />
+//           </Route>
+//           <Route path="/Lobby">
+//             <Lobby />
+//           </Route>
+//           <Route path="/Question">
+//             <Questiondisplay_layout />
+//           </Route>
+
+//           <Route path="/New_Question">
+//             <Newquestion_layout />
+//           </Route>
+          
+          
+//         </Switch>
+//       </div>
+//     </Router> }
+//     </div>
+//   )
+
